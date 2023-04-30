04ddd67cb081cc191f972b5222a0a1fdbf77e057
Hatem Xhezairi <hatem@uptechshpk.com>
2023-04-30 19:09:01 +0200

    candidate controller

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,47 +1,82 @@
 class CandidatesController < ApplicationController
+
+  before_action :authorized
   
   def index
-    candidates = Candidate.all.order("id DESC")
-    render json: candidates.map{ |candidate| 
-      candidate.as_json.merge(image: url_for(candidate.image))
+    candidates = Candidate.all
+
+    render json: candidates.map { |candidate|
+      candidate.as_json.merge(image: candidate_image(candidate))
     }
   end
 
   def show
-    candidate = Candidate.find(params[:id])
-    render json: candidate.as_json.merge(image: url_for(candidate.image))
-  end
-
-  def new
-    candidate = Candidate.new
-    render json: candidate
-  end
-
-  def edit
-    candidate = Candidate.find(params[:id])
-    render json: candidate
+    candidate = find_candidate
+    
+    render json: candidate.as_json.merge(image: candidate_image(candidate))
+    
+    rescue ActiveRecord::RecordNotFound => e
+        render json: { message: e.message }
   end
 
   def create
-    candidate = Candidate.create(candidate_params)
-    # candidate.images.attach(params[:images])
-    render json: candidate if candidate.save
+    candidate = Candidate.create! candidate_params
+
+    if candidate.valid?
+      render json: candidate.as_json.merge(image: candidate_image(candidate))
+    else
+      render json: { message: "Something went wrong" }
+    end
+
+    rescue ActiveRecord::RecordInvalid => e
+      render json: { message: e.message }
   end
 
   def update
-    candidate = Candidate.find(params[:id])
-    render json: candidate if candidate.update(candidate_params)
+    candidate = find_candidate
+    
+    if candidate
+      data = candidate_params.to_h.except(:user_id, :job_id)
+
+      if data[:image]
+        # remove current image
+        candidate.image.purge if candidate.image.attached?
+
+        # attach new image
+        candidate.image.attach(data[:image])
+
+        render json: candidate.as_json.merge(image: candidate_image(candidate))
+      else
+        render json: candidate.as_json.merge(image: candidate_image(candidate))  
+      end
+    end
+
+    rescue ActiveRecord::RecordNotFound => e
+        render json: { message: e.message }
+
+    rescue ArgumentError => e
+        render json: { message: e.message }
   end
 
   def destroy
-    candidate = Candidate.find(params[:id])
-    candidate.destroy
-
-    render json: "successfully deleted"
+    candidate = find_candidate 
+    render json: { message: "Successfully deleted" } if candidate.destroy!
+    
+    rescue ActiveRecord::RecordNotFound => e
+        render json: { message: e.message }
   end
 
   private
+
+    def find_candidate
+      Candidate.find(params[:id])
+    end
+
+    def candidate_image(candidate)
+      candidate.image.attached? ? url_for(candidate.image) : nil
+    end
+
     def candidate_params
-      params.permit(:name, :surname, :email, :location, :phone, :job_id, :image)
+      params.permit(:user_id, :job_id, :image)
     end
 end
--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,4 +1,16 @@
 class Candidate < ApplicationRecord
   belongs_to :user
   belongs_to :job
+
+  has_one_attached :image
+  # ActiveStorage::Blob.count
+
+  validates_uniqueness_of :user_id, scope: :job_id
+
+  before_destroy :remove_image
+
+  def remove_image
+    self.image.purge if self.image.attached?
+  end
+
 end
